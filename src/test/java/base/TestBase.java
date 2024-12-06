35ac557f53d27e3296d5e1a2f8d15f51f305af7b
josue <josuerogelio345@gmail.com>
2024-12-05 18:26:04 -0600

    test: add chromedriver

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -1,12 +1,17 @@
 package base;
 
 import java.io.ByteArrayInputStream;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -20,6 +25,7 @@ public class TestBase extends DriverManager {
 	// Setup and TearDown
 	
 	public WebDriver driver;
+	Capabilities chromeOptions = new DesiredCapabilities();
 	PropertyReader pr = new PropertyReader();
 	
 	public TestBase() {
@@ -27,7 +33,10 @@ public class TestBase extends DriverManager {
 		this.driver = super.getDriver();
 		
 	}
-	
+
+
+
+
 	@BeforeMethod(alwaysRun = false)
 	public void setup() {
 		//Read from properties file and set the WebDriver
@@ -37,16 +46,17 @@ public class TestBase extends DriverManager {
 			if(PropertyReader.readItem("browser").equalsIgnoreCase("chrome")) {
 				//updates chomedriver version to 64bits
 				System.getProperty("java.classpath");
-				System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win64\\chromedriver.exe");
+				System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+ "/chromedriver.exe");
 				driver = new ChromeDriver();
-				
+				//driver = new RemoteWebDriver(new URL("http://192.168.0.135:4444"),  chromeOptions);
 				
 			}
 			else {
 				try {
 					throw new Exception("Browser Driver is not supported");
 				}catch (Exception e) {
-					
+
+
 					Log.error("No Compatible browser found", e);
 					
 				}
